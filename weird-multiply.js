1ebe52c89509649413b3de5494f04fcc1eff69d9
chrisantoni <tonichen184@gmail.com>
2017-08-08 14:55:25 +0700

    commit1

---
+++ weird-multiply.js
@@ -0,0 +1,22 @@
+
+function weirdMultiply(num) {
+  var numArr = String(num).split("");
+  var temp;
+  if(numArr.length === 1)return num;
+  while(numArr.length > 1){
+      temp = 1;
+    for(var i=0;i<numArr.length;i++){
+        temp *= +numArr[i];
+    }
+
+    numArr = String(temp).split("");
+  }
+
+  return Number(numArr.join(""));
+
+}
+console.log(weirdMultiply(39)); // -> 3 * 9 = 27 -> 2 * 7 = 14 -> 1 * 4 = 4 // result = 4
+console.log(weirdMultiply(999)); // -> 9 * 9 * 9 = 729 -> 7 * 2 * 9 = 126 -> 1 * 2 * 6 = 12 -> 1 * 2 = 2 // result = 2
+console.log(weirdMultiply(3)); // -> 3 karena telah satu digit // result = 3
+
+
